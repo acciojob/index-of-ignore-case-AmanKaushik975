380a24e9caf44d4604b680b78f59f012975bdebc
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-04-30 23:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(!s2.includes(s1)){
+		return -1;
+	}else{
+		return s1.indexOf(s2);
+	}
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
